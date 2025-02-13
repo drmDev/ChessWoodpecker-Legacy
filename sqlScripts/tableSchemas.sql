0297b0d4e85a1ecde0868c963125ff97185d2c9d
Derek Monturo <dmontur1@gmail.com>
2025-02-12 21:42:11 -0500

    condense sql

--- sqlScripts/tableSchemas.sql
+++ sqlScripts/tableSchemas.sql
@@ -14,5 +14,14 @@ CREATE TABLE user_progress (
     user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
     puzzle_id INT REFERENCES puzzles(puzzle_id) ON DELETE CASCADE,
     completed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE (user_id, puzzle_id)  -- Ensures a user can complete a puzzle only once
+    UNIQUE (user_id, puzzle_id)
+);
+
+CREATE TABLE user_sessions (
+    session_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    current_puzzle_index INT NOT NULL,
+    total_time BIGINT NOT NULL,
+    puzzle_times JSONB NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- sqlScripts/userSessions.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE user_sessions (
-    session_id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
-    current_puzzle_index INT NOT NULL,
-    total_time BIGINT NOT NULL,
-    puzzle_times JSONB NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
